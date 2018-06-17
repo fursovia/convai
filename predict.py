a30aec377549b8e7938aec60bf2f6d4ff3e3a1ca
Fursov Ivan Andreevich <i.fursov@tinkoff.ru>
2018-06-17 14:31:11 +0300

    added dict

--- predict.py
+++ predict.py
@@ -16,7 +16,28 @@ parser.add_argument('--model_dir', default='experiments',
 parser.add_argument('--data_dir', default='data',
                     help="Directory containing the dataset")
 
-some_dict = {}
+some_dict = {'eval_labels': ("that sounds awesome . i'm just washing my cars and my dogs on my day off .",),
+             'label_candidates': ('i read anything with spiderman !',
+                                  'i like jazz but my best friend and now roommate plays rock all the time',
+                                  "yea that's the problem with blueberries lol",
+                                  "i'm okay , eating some ice cream before bed .",
+                                  'pizza with black olives .',
+                                  "i'm very impressed ! you seem to have it all together .",
+                                  'i will need a bedroom painted in purple and lime green though .',
+                                  'i watch kids on the playground . i teach third grade .',
+                                  'i play video games . would love to make a living out of it',
+                                  'dang that kinda weird . well do you have a pet',
+                                  'well , anyone can sing , lessons help',
+                                  "she's 9 . i love them all . what work did you do before retiring ?",
+                                  "hello , i'm doing very well , thank you . i'm getting married soon .",
+                                  'oh i want to visit there very badly , but my cancer just went into remission',
+                                  'you are married ? i will finish high school in two years .',
+                                  "do you have pet ? i have 5 children and that's more then enough . all boys",
+                                  'hello ! you should friend me on facebook .',
+                                  'what games do you play ?',
+                                  'chemistry was my favorite subject , did you get bullied at all ?',
+                                  "that sounds awesome . i'm just washing my cars and my dogs on my day off ."),
+             'text': "your persona: i like to go to country concerts on weekends.\nyour persona: i own two vintage mustangs.\nyour persona: my favorite music is country.\nyour persona: i've two dogs.\nhello there ! i'm reginald , or reggie if you prefer . how are you today ?\nhowdy , hey ! i'm pretty good\nthat's great . i'm doing good as well . the grandkids are coming over later"}
 
 if __name__ == '__main__':
     tf.reset_default_graph()
