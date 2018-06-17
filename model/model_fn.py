548baf6a0193ff1e76afe939692cc00cc03e6649
Fursov Ivan Andreevich <i.fursov@tinkoff.ru>
2018-06-17 13:22:54 +0300

    test

--- model/model_fn.py
+++ model/model_fn.py
@@ -25,7 +25,7 @@ def build_model(is_training, sentences, params):
         if params.pretrained:
             weights_initializer = get_embeddings(params)
         else:
-            weights_initializer = None
+            weights_initializer = tf.truncated_normal_initializer(stddev=0.001)
     else:
         weights_initializer = None  # don't spend time on embeddings initialization
 
@@ -36,40 +36,39 @@ def build_model(is_training, sentences, params):
                                            dtype=tf.float32)
 
         context = tf.nn.embedding_lookup(embedding_matrix, context)
-        # question = tf.nn.embedding_lookup(embedding_matrix, question)
-        # reply = tf.nn.embedding_lookup(embedding_matrix, reply)
-        # personal_info = tf.nn.embedding_lookup(embedding_matrix, personal_info)
-        # personal_info = tf.reshape(personal_info, [-1, 5, 20, params.embedding_size])
+        question = tf.nn.embedding_lookup(embedding_matrix, question)
+        reply = tf.nn.embedding_lookup(embedding_matrix, reply)
+        personal_info = tf.nn.embedding_lookup(embedding_matrix, personal_info)
+        personal_info = tf.reshape(personal_info, [-1, 5, 20, params.embedding_size])
 
-        # question = tf.reduce_mean(question, axis=1)  # [None, 300]
-        # reply = tf.reduce_mean(reply, axis=1)  # [None, 300]
-        # personal_info = tf.reduce_mean(personal_info, axis=2)  # [None, 5, 300]
+        question = tf.reduce_mean(question, axis=1)  # [None, 300]
+        reply = tf.reduce_mean(reply, axis=1)  # [None, 300]
+        personal_info = tf.reduce_mean(personal_info, axis=2)  # [None, 5, 300]
 
     with tf.name_scope("context_attention"):
-        # context_output = attention(context, 50)
-        context_output = tf.reduce_mean(context, axis=1)
+        context_output = attention(context, 50)
 
-    # with tf.name_scope("closest_fact"):
-    #     dot_product = tf.matmul(tf.expand_dims(reply, 1), personal_info, transpose_b=True)  # [None, 5]
-    #     dot_product = tf.reshape(dot_product, [-1, 5])
-    #     max_dot_product = tf.reduce_max(dot_product, axis=1, keepdims=True)  # how close?
-    #     max_fact_id = tf.argmax(dot_product, axis=1)
-    #     mask = tf.cast(tf.one_hot(max_fact_id, 5), tf.bool)
-    #     closest_info = tf.boolean_mask(personal_info, mask, axis=0)
+    with tf.name_scope("closest_fact"):
+        dot_product = tf.matmul(tf.expand_dims(reply, 1), personal_info, transpose_b=True)  # [None, 5]
+        dot_product = tf.reshape(dot_product, [-1, 5])
+        max_dot_product = tf.reduce_max(dot_product, axis=1, keepdims=True)  # how close?
+        max_fact_id = tf.argmax(dot_product, axis=1)
+        mask = tf.cast(tf.one_hot(max_fact_id, 5), tf.bool)
+        closest_info = tf.boolean_mask(personal_info, mask, axis=0)
 
-    # concatenated = tf.concat([context_output, question, reply, closest_info, max_dot_product], axis=1)
+    concatenated = tf.concat([context_output, question, reply, closest_info, max_dot_product], axis=1)
 
     with tf.variable_scope('fc_0'):
-        dense0 = tf.layers.dense(context_output, 512)
+        dense0 = tf.layers.dense(concatenated, 1024, activation=tf.nn.relu)
 
-    # with tf.variable_scope('fc_1'):
-    #     dense1 = tf.layers.dense(dense0, 512, activation=tf.nn.elu)
-    #
-    # with tf.variable_scope('fc_2'):
-    #     dense2 = tf.layers.dense(dense1, 256, activation=tf.nn.elu)
+    with tf.variable_scope('fc_1'):
+        dense1 = tf.layers.dense(dense0, 512, activation=tf.nn.relu)
+
+    with tf.variable_scope('fc_2'):
+        dense2 = tf.layers.dense(dense1, 256, activation=tf.nn.relu)
 
     with tf.variable_scope('fc_3'):
-        dense3 = tf.layers.dense(dense0, 2)
+        dense3 = tf.layers.dense(dense2, 2)
 
     return dense3
 
@@ -116,14 +115,12 @@ def model_fn(features, labels, mode, params):
 
     tf.summary.scalar('accuracy', acc_op)
 
-    optimizer = tf.train.AdamOptimizer(params.learning_rate)
-
     global_step = tf.train.get_global_step()  # number of batches seen so far
     train_op = tf.contrib.layers.optimize_loss(
         loss=loss,
         global_step=global_step,
         learning_rate=params.learning_rate,
-        optimizer=optimizer
+        optimizer=tf.train.AdamOptimizer()
     )
 
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
