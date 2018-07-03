ebc3e3569afbc296eb3bfc6fff1d003a5e039a48
tcsbank\i.a.anokhin <i.a.anokhin@tinkoff.ru>
2018-07-03 10:31:01 +0300

    'update'

---
+++ server/main.py
@@ -0,0 +1,200 @@
+import asyncio
+import uvloop
+import sqlite3
+import aiohttp
+import os
+import json
+import datetime
+
+def check_db(connection):
+    return connection.execute(
+        '''
+            select count(1)
+              from sqlite_master
+             where type = ?
+               and name = ?;
+        ''',
+        ('table', 'messages')
+    ).fetchone() == (1,)
+
+
+def setup_db(connection):
+    connection.execute('''
+        create table messages (
+            chat_id bigint,
+            message_id bigint,
+            source text,
+            text text,
+            created_at timestamp
+        );
+    ''')
+
+
+def get_context(connection, chat_id, timestamp, limit=3):
+    return connection.execute(
+        '''
+            select text
+              from messages
+             where chat_id = ?
+               and created_at < ?
+             order by created_at desc
+             limit ?;
+        ''',
+        (chat_id, timestamp, limit)
+    ).fetchall()[::-1]
+
+
+def get_facts(connection, chat_id):
+    lines = connection.execute(
+        '''
+            select text
+              from messages
+             where chat_id = ?
+             order by created_at
+             limit 1;
+        ''',
+        (chat_id,)
+    ).fetchone()[0].split('\n')
+    if lines[0].strip().lower() != '/start':
+        return []
+    return lines[1:]
+
+
+async def get_updates(url, retry_timeout):
+    while True:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    j = await resp.json()
+                    if j.get('ok') == True and len(j['result']) > 0:
+                        return j['result']
+                    await asyncio.sleep(retry_timeout)
+                    continue
+
+
+async def send_message(url, chat_id, text):
+    j = {
+        'chat_id': chat_id,
+        'text': json.dumps({
+            'text': text
+        })
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=j) as resp:
+            if resp.status == 200:
+                return True
+
+
+def save_message(connection, chat_id, message_id, text):
+    created_at = datetime.datetime.now()
+    connection.execute(
+        '''
+            insert into messages (
+                chat_id,
+                message_id,
+                source,
+                text,
+                created_at
+            ) values (?, ?, ?, ?, ?);
+        ''',
+        (
+            chat_id,
+            message_id,
+            'user',
+            text,
+            created_at
+        )
+    )
+    return created_at
+
+
+def save_answer(connection, chat_id, text):
+    created_at = datetime.datetime.now()
+    connection.execute(
+        '''
+            insert into messages (
+                chat_id,
+                source,
+                text,
+                created_at
+            ) values (?, ?, ?, ?);
+        ''',
+        (
+            chat_id,
+            'bot',
+            text,
+            created_at
+        )
+    )
+    return created_at
+
+
+def form_data(connection, chat_id, text, created_at):
+    return {
+        'context': get_context(
+            connection,
+            chat_id,
+            created_at,
+        ),
+        'question': text,
+        'facts': get_facts(
+            connection,
+            chat_id
+        )
+    }
+
+
+async def process_updates(updates, connection, loop, send_message_url):
+    answers = []
+    for update in updates:
+        chat_id = update['message']['chat']['id']
+        message_id = update['message']['message_id']
+        text = update['message']['text']
+        created_at = save_message(connection, chat_id, message_id, text)
+        answers.append((
+            chat_id,
+            loop.run_in_executor(
+                None,
+                get_answer,
+                form_data(connection, chat_id, text, created_at)
+            )
+        ))
+    sends = []
+    for chat_id, answer in answers:
+        answer_text = await answer
+        sends.append(send_message(send_message_url, chat_id, answer_text))
+        save_answer(connection, chat_id, answer_text)
+    for send in sends:
+        await send
+
+
+async def main(loop, connection, get_updates_url, send_message_url):
+    while True:
+        asyncio.ensure_future(
+            process_updates(
+                await get_updates(get_updates_url, 0.5),
+                connection, loop, send_message_url
+            ),
+            loop=loop
+        )
+
+
+def get_answer(data):
+    return '¯\_(ツ)_/¯'
+
+
+if __name__ == '__main__':
+    bot_token = os.environ['BOT_TOKEN']
+    connection = sqlite3.connect('loopai.db')
+    if not check_db(connection):
+        setup_db(connection)
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        main(
+            loop,
+            connection,
+            f'https://2258.lnsigo.mipt.ru/bot{bot_token}/getUpdates',
+            f'https://2258.lnsigo.mipt.ru/bot{bot_token}/sendMessage'
+        )
+    )
