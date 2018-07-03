a52532d6f0d4ee6a7a7b5496f3960d5a898edb60
tcsbank\i.a.anokhin <i.a.anokhin@tinkoff.ru>
2018-07-03 22:47:53 +0300

    Merge branch 'master' of https://github.com/fursovia/convai
    # Conflicts:
    #	experiments/params.json
    #	model/architectures.py
    #	train.py

--- model/model_fn.py
+++ model/model_fn.py
@@ -2,6 +2,7 @@
 
 import tensorflow as tf
 from model.architectures import build_model
+from model.loss import get_loss
 
 def model_fn(features, labels, mode, params):
 
@@ -10,32 +11,45 @@ def model_fn(features, labels, mode, params):
     with tf.variable_scope('model'):
         logits = build_model(is_training, features, params)
 
-    preds = tf.nn.softmax(logits)
+        print('logits', logits[0].shape, logits[0].shape)
 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = {'y_prob': preds[:, 1],
-                       'y_pred': tf.argmax(preds, axis=1)}
+    if params.loss_type == 'usual':
+        loss = get_loss(labels, logits, params)
+        preds = tf.nn.softmax(logits)
 
-        return tf.estimator.EstimatorSpec(mode=mode,
-                                          predictions=predictions,
-                                          export_outputs={
-                                              'predict': tf.estimator.export.PredictOutput(predictions)
-                                          })
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            predictions = {'y_prob': preds[:, 1],
+                           'y_pred': tf.argmax(preds, axis=1)}
 
-    one_hot_labels = tf.one_hot(labels, 2)
+            return tf.estimator.EstimatorSpec(mode=mode,
+                                              predictions=predictions,
+                                              export_outputs={
+                                                  'predict': tf.estimator.export.PredictOutput(predictions)
+                                              })
 
-    loss = tf.reduce_mean(
-        tf.losses.softmax_cross_entropy(onehot_labels=one_hot_labels, logits=logits)
-    )
+        if mode == tf.estimator.ModeKeys.EVAL:
+            acc, acc_op = tf.metrics.accuracy(labels=labels, predictions=tf.argmax(preds, axis=1), name='acc')
+            with tf.variable_scope("metrics"):
+                eval_metric_ops = {"accuracy": (acc, acc_op)}
 
-    if mode == tf.estimator.ModeKeys.EVAL:
-        acc, acc_op = tf.metrics.accuracy(labels=labels, predictions=tf.argmax(preds, axis=1), name='acc')
-        with tf.variable_scope("metrics"):
-            eval_metric_ops = {"accuracy": (acc, acc_op)}
+            return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    else:
+        loss, fraction, p_at_k, mrr = get_loss(labels, logits, params)
+        if mode == tf.estimator.ModeKeys.EVAL:
+            with tf.variable_scope("metrics"):
+                eval_metric_ops = {"precision_at_K": tf.metrics.mean(p_at_k),
+                                   "MRR": tf.metrics.mean(mrr),
+                                   "fraction_positive_triplets": tf.metrics.mean(fraction)}
 
-    # tf.summary.scalar('accuracy', acc_op)
+            return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+        tf.summary.scalar('loss', loss)
+        tf.summary.scalar('fraction_positive_triplets', fraction)
+        tf.summary.scalar('precision_at_K', p_at_k)
+        tf.summary.scalar('MRR', mrr)
+
+        # tf.summary.scalar('accuracy', acc_op)
 
     global_step = tf.train.get_global_step()  # number of batches seen so far
     train_op = tf.contrib.layers.optimize_loss(
