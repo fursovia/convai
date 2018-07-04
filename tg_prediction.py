933c732d0f09ab8b13caeec2e35dc623b8143e96
tcsbank\i.a.anokhin <i.a.anokhin@tinkoff.ru>
2018-07-04 14:13:57 +0300

    'fixed predictor'

--- tg_prediction.py
+++ tg_prediction.py
@@ -7,20 +7,30 @@ import numpy as np
 from model.utils import inference_time
 from knn import KNeighborsClassifier
 
+import tensorflow as tf
+import os
+from model.utils import Params
+from model.model_fn import model_fn
+import pickle
+import numpy as np
+from model.utils import inference_time
+from knn import KNeighborsClassifier
+
 
 class serving_input_fn:
     def __init__(self):
         self.features = {'cont': tf.placeholder(tf.int64, shape=[None, 140]),
-           'quest': tf.placeholder(tf.int64, shape=[None, 140]),
-           'resp': tf.placeholder(tf.int64, shape=[None, 140]),
-           'facts': tf.placeholder(tf.int64, shape=[None, 5*140]) }
-#         self.features = tf.placeholder(tf.int64, shape=[None, 8, 140])
-#         self.quest = tf.placeholder(tf.int64, shape=[None, 140])
-#         self.resp = tf.placeholder(tf.int64, shape=[None, 140])
-#         self.facts = tf.placeholder(tf.int64, shape=[None, 5, 140])
+                         'quest': tf.placeholder(tf.int64, shape=[None, 140]),
+                         'resp': tf.placeholder(tf.int64, shape=[None, 140]),
+                         'facts': tf.placeholder(tf.int64, shape=[None, 5 * 140])}
+        #         self.features = tf.placeholder(tf.int64, shape=[None, 8, 140])
+        #         self.quest = tf.placeholder(tf.int64, shape=[None, 140])
+        #         self.resp = tf.placeholder(tf.int64, shape=[None, 140])
+        #         self.facts = tf.placeholder(tf.int64, shape=[None, 5, 140])
 
-        self.receiver_tensors = {'text': self.features}
+        self.receiver_tensors = self.features  # {'text': self.features}
         self.receiver_tensors_alternatives = None
+
 
 class pred_agent():
     def __init__(self, args, raw_utterances, train_embeddings_path):
@@ -39,7 +49,6 @@ class pred_agent():
         self.uni2idx = pickle.load(open(uni2idx_path, 'rb'))
         self.bi2idx = pickle.load(open(bi2idx_path, 'rb'))
         self.char2idx = pickle.load(open(char2idx_path, 'rb'))
-
 
     def create_model(self):
         tf.reset_default_graph()
@@ -67,7 +76,6 @@ class pred_agent():
         train_embeddings = pickle.load(open(self.train_embeddings_path, 'rb'))
         self.knn_model = KNeighborsClassifier(n_neighbors=45).fit(train_embeddings, np.zeros_like(train_embeddings))
 
-
     def choose_from_knn(self, q_embeddings):
         indicies, _ = self.knn_model.get_labels_and_distances(q_embeddings)
         chosen = self.raw_utterances[indicies]
@@ -76,16 +84,19 @@ class pred_agent():
     def predict(self, super_dict):
         vocabs = [self.uni2idx, self.bi2idx, self.char2idx]
 
-        data_to_predict_knn = inference_time(super_dict, np.zeros((1, 140)), vocabs, 1)  # ['q_emb']
-        print(data_to_predict_knn)
-        test_predictions_knn = self.predictor({'text': {'cont': data_to_predict_knn[:,0].reshape(-1, 140),
-                                              'quest': data_to_predict_knn[:,1].reshape(-1, 140),
-                                              'resp': data_to_predict_knn[:,2].reshape(-1, 140),
-                                              'facts': data_to_predict_knn[:,3:].reshape(-1, 5*140)}})
+        data_to_predict_knn = inference_time(super_dict, np.zeros((1, 140)), vocabs, 1)
+        #         print(data_to_predict_knn)
+        test_predictions_knn = self.predictor({'cont': data_to_predict_knn[:, 0].reshape(-1, 140),
+                                               'quest': data_to_predict_knn[:, 1].reshape(-1, 140),
+                                               'resp': data_to_predict_knn[:, 2].reshape(-1, 140),
+                                               'facts': data_to_predict_knn[:, 3:].reshape(-1, 5 * 140)})
+
+        #         print('test_predictions_knn', test_predictions_knn)
 
         qemb = []
-        for i, p in enumerate(test_predictions_knn):
-            qemb.append(p['hist_embed'])
+        for p in test_predictions_knn['hist_emb']:
+            #             print('p', p)
+            qemb.append(p)
         qemb = np.array(qemb, float).reshape(-1, 300)
 
         chosen = self.choose_from_knn(qemb)
