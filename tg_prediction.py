e18ebb51346d24745f8b9268428ffd73b76b0100
fursovia <CS9mwwux>
2018-07-04 21:36:09 +0300

    changed param

--- tg_prediction.py
+++ tg_prediction.py
@@ -82,9 +82,9 @@ class pred_agent():
 
     def predict(self, super_dict):
         vocabs = [self.uni2idx, self.bi2idx, self.char2idx]
-        print('vocabs', vocabs)
+        print('vocabs')
         data_to_predict_knn = inference_time(super_dict, np.zeros((1, 140)), vocabs, 1)
-        print('predict this data', data_to_predict_knn)
+        print('predict this data', data_to_predict_knn[0])
         test_predictions_knn = self.predictor({'cont': data_to_predict_knn[:, 0].reshape(-1, 140),
                                                'quest': data_to_predict_knn[:, 1].reshape(-1, 140),
                                                'resp': data_to_predict_knn[:, 2].reshape(-1, 140),
