7c58d2a39cab616daf3d266186cfddaa4279f92e
tcsbank\i.a.anokhin <i.a.anokhin@tinkoff.ru>
2018-07-06 13:39:20 +0300

    'model update'

--- validate.py
+++ validate.py
@@ -25,7 +25,7 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='/data/i.fursov/convai/last_exp')
 parser.add_argument('--data_dir', default='/data/i.fursov/convai/last_data')
-parser.add_argument('--рш', default='Y')
+parser.add_argument('--emb_dim', type=int, default=300)
 
 
 if __name__ == '__main__':
